1b664804d0b51bae8349520f75057f7c5cdd50aa
Mathew Huusko V <mhuusko5@gmail.com>
2014-02-02 17:05:04 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -177,16 +177,15 @@ $window.load(function() {
         })());
     }, 200);
 
-    setTimeout(function() {
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-43318814-4']);
-        _gaq.push(['_trackPageview']);
+    if (window.location.protocol != 'file:') {
+        setTimeout(function() {
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == window.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    }, 5000);
+            ga('create', 'UA-47723827-1', 'mhuusko5.com');
+            ga('send', 'pageview');
+        }, 1000);
+    }
 });
