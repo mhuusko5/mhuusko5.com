d85396c7ae822736dc5e872c05461635521ce08b
Mathew Huusko V <mhuusko5@gmail.com>
2014-04-13 18:37:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -322,24 +322,6 @@ $window.load(function() {
     } else {
         showContent();
     }
-
-    if (window.location.protocol != 'file:') {
-        setTimeout(function() {
-            (function(i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function() {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-47723827-1', 'mhuusko5.com');
-            ga('send', 'pageview');
-        }, 2000);
-    }
     
     $window.trigger('resize');
 });
